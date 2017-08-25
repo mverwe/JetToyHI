3339d6fbfe70cd4ef850ce2822d2408b36769c8e
Marta <marta.verweij@cern.ch>
2017-08-25 10:03:44 +0200

    CS variations of alpha

---
+++ plot/plotJetEnergyScaleCS.C
@@ -0,0 +1,143 @@
+//Quick dirty plotting macro for jet response from trees of JetToyHI framework
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <fstream>
+
+#include "TCanvas.h"
+#include "TFile.h"
+#include "TH3F.h"
+#include "TH1F.h"
+#include "TH1D.h"
+#include "TGraphAsymmErrors.h"
+#include "TLegend.h"
+
+using namespace std;
+
+int markerColor[11] = {1,kRed+1,4,kGreen+3,kOrange+7,kGray+2,kRed+5,kAzure+10,kGreen+4,kYellow+2,kGreen};
+int markerStyle[8] = {20,21,33,34,24,25,27,28};
+
+TH1F *DrawFrame(double xmin = 0., double xmax = 1., double ymin = 0., double ymax = 1., TString xTitle = "x", TString yTitle = "y", bool setMargins = true);
+
+void plotJetEnergyScaleCS(TString str = "JetToyHIResult.root") {
+
+  TFile *f = new TFile(str.Data());
+  TTree *tr = dynamic_cast<TTree*>(f->Get("jetTree"));
+
+  tr->SetLineWidth(2);
+
+  int nEvt = tr->GetEntriesFast();
+
+  const int ncs = 5;
+ 
+  TString strLeg[ncs] = {"CS #alpha=-2",
+                        "CS #alpha=-1",
+                        "CS #alpha=0",
+                        "CS #alpha=1",
+                        "CS #alpha=2",
+  };
+
+
+  TH1 *hResponsePt[ncs] = {0};
+  TH1 *hResponseM[ncs]  = {0};
+
+  //----------------------------------------------------------------
+  // plot the jet pT response
+  //----------------------------------------------------------------
+  TCanvas *c1 = new TCanvas("c1","c1",450,400);
+  gPad->SetLogy();
+  tr->SetLineColor(1);
+  tr->Draw("csJetPt/sigJetPt>>hCSJetResponsePt(100,0.,2.)","sigJetPt>120. && abs(sigJetEta)<2.3");
+  hResponsePt[0] = dynamic_cast<TH1*>(gDirectory->Get("hCSJetResponsePt"));
+
+  for(int i = 0; i<ncs; ++i) {
+    tr->Draw(Form("csJet_%dPt/sigJetPt>>hCS%dJetResponsePt(100,0.,2.)",i,i),"sigJetPt>120. && abs(sigJetEta)<2.3","same");
+    hResponsePt[i] = dynamic_cast<TH1*>(gDirectory->Get(Form("hCS%dJetResponsePt",i)));
+  }
+
+  for(int i = 0; i<ncs; ++i) {
+    if(!hResponsePt[i]) continue;
+    hResponsePt[i]->Sumw2();
+    if(nEvt>0) hResponsePt[i]->Scale(1./(double)nEvt);
+  }
+
+  TH1F *fr1 = DrawFrame(0.,2.,5e-5,5e-1,"p_{T,sub}/p_{T,gen}","N/N_{evt}");
+  TLegend *leg1 = new TLegend(0.23,0.6,0.45,0.93);
+  leg1->SetFillColor(10);
+  leg1->SetBorderSize(0);
+  leg1->SetFillStyle(0);
+  leg1->SetTextSize(0.05);
+  for(int i = 0; i<ncs; ++i) {
+    if(!hResponsePt[i]) continue;
+    hResponsePt[i]->SetLineColor(markerColor[i]);
+    hResponsePt[i]->SetMarkerColor(markerColor[i]);
+    hResponsePt[i]->SetMarkerStyle(markerStyle[i]);
+    hResponsePt[i]->Draw("same");
+    leg1->AddEntry(hResponsePt[i],strLeg[i].Data(),"p");
+  }
+  leg1->Draw();
+
+  //----------------------------------------------------------------
+  // plot the jet mass response
+  //----------------------------------------------------------------
+  TCanvas *c4 = new TCanvas("c4","c4",450,400);
+  gPad->SetLogy();
+  tr->Draw("csJetM/sigJetM>>hCSJetResponseM(100,0.,5.)","sigJetPt>120. && abs(sigJetEta)<2.3");
+  hResponseM[0] = dynamic_cast<TH1*>(gDirectory->Get("hCSJetResponseM"));
+
+  for(int i =0; i<nsk; ++i) {
+    tr->Draw(Form("csJet_%dM/sigJetM>>hCS%dJetResponseM(100,0.,2.)",i,i),"sigJetPt>120. && abs(sigJetEta)<2.3","same");
+    hResponseM[i] = dynamic_cast<TH1*>(gDirectory->Get(Form("hCS%dJetResponseM",i)));
+  }
+
+  for(int i = 0; i<ncs; ++i) {
+    if(!hResponseM[i]) continue;
+    hResponseM[i]->Sumw2();
+    if(nEvt>0) hResponseM[i]->Scale(1./(double)nEvt);
+  }
+  
+  TH1F *fr2 = DrawFrame(0.,5.,5e-5,5e-1,"M_{sub}/M_{gen}","N/N_{evt}");
+  TLegend *leg2 = new TLegend(0.5,0.6,0.93,0.93);
+  leg2->SetFillColor(10);
+  leg2->SetBorderSize(0);
+  leg2->SetFillStyle(0);
+  leg2->SetTextSize(0.05);
+  for(int i = 0; i<ncs; ++i) {
+    if(!hResponseM[i]) continue;
+    hResponseM[i]->SetLineColor(markerColor[i]);
+    hResponseM[i]->SetMarkerColor(markerColor[i]);
+    hResponseM[i]->SetMarkerStyle(markerStyle[i]);
+    hResponseM[i]->Draw("same");
+    leg2->AddEntry(hResponseM[i],strLeg[i].Data(),"p");
+  }
+  leg2->Draw();
+   
+}
+
+TH1F *DrawFrame(double xmin, double xmax, double ymin, double ymax, TString xTitle, TString yTitle, bool setMargins) {
+
+  if(setMargins) {
+    gPad->SetLeftMargin(0.22);
+    gPad->SetBottomMargin(0.15);
+    gPad->SetRightMargin(0.1);//0.05);
+    gPad->SetTopMargin(0.05);
+  }
+
+  TH1F *frame = gPad->DrawFrame(xmin,ymin,xmax,ymax);
+  frame->SetXTitle(xTitle.Data());
+  frame->SetYTitle(yTitle.Data());
+  frame->GetXaxis()->SetLabelSize(0.05);
+  frame->GetYaxis()->SetLabelSize(0.05);
+  frame->GetXaxis()->SetTitleSize(0.06);
+  frame->GetYaxis()->SetTitleSize(0.06);
+  frame->GetXaxis()->SetTitleOffset(1.0);
+  frame->GetYaxis()->SetTitleOffset(1.3);
+  frame->GetXaxis()->CenterTitle(true);
+  frame->GetYaxis()->CenterTitle(true);
+
+  gPad->SetTicks(1,1);
+
+  return frame;
+}
---
+++ runCSVariations.cc
@@ -0,0 +1,197 @@
+#include <iostream>
+#include <chrono>
+
+#include "TFile.h"
+#include "TTree.h"
+
+#include "fastjet/PseudoJet.hh"
+#include "fastjet/ClusterSequenceArea.hh"
+
+#include "include/ProgressBar.h"
+
+#include "PU14/EventMixer.hh"
+#include "PU14/CmdLine.hh"
+#include "PU14/PU14.hh"
+
+#include "include/jetCollection.hh"
+#include "include/csSubtractor.hh"
+#include "include/csSubtractorFullEvent.hh"
+#include "include/skSubtractor.hh"
+#include "include/softDropGroomer.hh"
+#include "include/treeWriter.hh"
+#include "include/jetMatcher.hh"
+#include "include/randomCones.hh"
+#include "include/Angularity.hh"
+
+using namespace std;
+using namespace fastjet;
+
+// ./runCSVariations -hard  /eos/project/j/jetquenching/JetWorkshop2017/samples/pythia8/dijet120/PythiaEventsTune14PtHat120_0.pu14 -pileup  /eos/project/j/jetquenching/JetWorkshop2017/samples/thermal/Mult7000/ThermalEventsMult7000PtAv1.20_0.pu14 -nev 10
+
+int main (int argc, char ** argv) {
+
+  auto start_time = std::chrono::steady_clock::now();
+  
+  CmdLine cmdline(argc,argv);
+  // inputs read from command line
+  int nEvent = cmdline.value<int>("-nev",1);  // first argument: command line option; second argument: default value
+  //bool verbose = cmdline.present("-verbose");
+
+  std::cout << "will run on " << nEvent << " events" << std::endl;
+
+  // Uncomment to silence fastjet banner
+  ClusterSequence::set_fastjet_banner_stream(NULL);
+
+  //to write info to root tree
+  treeWriter trw("jetTree");
+
+  //Jet definition
+  double R                   = 0.4;
+  double ghostRapMax         = 6.0;
+  double ghost_area          = 0.005;
+  int    active_area_repeats = 1;
+  fastjet::GhostedAreaSpec ghost_spec(ghostRapMax, active_area_repeats, ghost_area);
+  fastjet::AreaDefinition area_def = fastjet::AreaDefinition(fastjet::active_area,ghost_spec);
+  fastjet::JetDefinition jet_def(antikt_algorithm, R);
+
+  double jetRapMax = 3.0;
+  fastjet::Selector jet_selector = SelectorAbsRapMax(jetRapMax);
+
+  Angularity width(1.,1.,R);
+  Angularity pTD(0.,2.,R);
+    
+  ProgressBar Bar(cout, nEvent);
+  Bar.SetStyle(-1);
+
+  EventMixer mixer(&cmdline);  //the mixing machinery from PU14 workshop
+
+  // loop over events
+  int iev = 0;
+  unsigned int entryDiv = (nEvent > 200) ? nEvent / 200 : 1;
+  while ( mixer.next_event() && iev < nEvent )
+  {
+    // increment event number    
+    iev++;
+
+    Bar.Update(iev);
+    Bar.PrintWithMod(entryDiv);
+
+    std::vector<fastjet::PseudoJet> particlesMerged = mixer.particles();
+
+    std::vector<double> eventWeight;
+    eventWeight.push_back(mixer.hard_weight());
+    eventWeight.push_back(mixer.pu_weight());
+
+    // cluster hard event only
+    std::vector<fastjet::PseudoJet> particlesBkg, particlesSig;
+    SelectorIsHard().sift(particlesMerged, particlesSig, particlesBkg); // this sifts the full event into two vectors of PseudoJet, one for the hard event, one for the underlying event
+    
+    //---------------------------------------------------------------------------
+    //   jet clustering
+    //---------------------------------------------------------------------------
+    
+    // run the clustering, extract the signal jets
+    fastjet::ClusterSequenceArea csSig(particlesSig, jet_def, area_def);
+    jetCollection jetCollectionSig(sorted_by_pt(jet_selector(csSig.inclusive_jets(10.))));
+
+    //---------------------------------------------------------------------------
+    //   background subtraction
+    //---------------------------------------------------------------------------
+    
+    //run jet-by-jet constituent subtraction on mixed (hard+UE) event
+    const int ncs = 5;
+    double alpha[ncs] = {-2.,-1.,0.,1.,2.};
+    std::vector<jetCollection> jetCollectionCSs;
+    std::vector<double> rho;
+    std::vector<double> rhom; 
+    for(int ics = 0; ics<ncs; ++ics) {
+      csSubtractor csSub(R, alpha[ics], -1, 0.005,ghostRapMax,jetRapMax);
+      csSub.setInputParticles(particlesMerged);
+      jetCollection jetCollectionCS(csSub.doSubtraction());
+
+      if(ics==3) {
+        //Background densities used by constituent subtraction
+        rho.push_back(csSub.getRho());
+        rhom.push_back(csSub.getRhoM());
+      }
+      jetCollectionCSs.push_back(jetCollectionCS);
+    }
+
+
+    //---------------------------------------------------------------------------
+    //   Groom the jets
+    //---------------------------------------------------------------------------
+    
+
+    //SoftDrop grooming classic for signal jets (zcut=0.1, beta=0)
+    softDropGroomer sdgSig(0.1, 0.0, R);
+    jetCollection jetCollectionSigSD(sdgSig.doGrooming(jetCollectionSig));
+    jetCollectionSigSD.addVector("zgSigSD",    sdgSig.getZgs());
+    jetCollectionSigSD.addVector("ndropSigSD", sdgSig.getNDroppedBranches());
+    jetCollectionSigSD.addVector("dr12SigSD",  sdgSig.getDR12());
+
+    //SoftDrop grooming classic for CS jets (zcut=0.1, beta=0)
+    std::vector<jetCollection> jetCollectionCSSDs;
+    
+    for(int ics = 0; ics<ncs; ++ics) {
+      softDropGroomer sdgCS(0.1, 0.0, R);
+      jetCollection jetCollectionCSSD(sdgCS.doGrooming(jetCollectionCSs[ics]));
+      jetCollectionCSSD.addVector(Form("csJetSD_%dzg",ics),    sdgCS.getZgs());
+      jetCollectionCSSD.addVector(Form("csJetSD_%dndrop",ics), sdgCS.getNDroppedBranches());
+      jetCollectionCSSD.addVector(Form("csJetSD_%ddr12",ics),  sdgCS.getDR12());
+
+      jetCollectionCSSDs.push_back(jetCollectionCSSD);
+      //std::cout << "n CS jets: " << jetCollectionCSs[ics].getJet().size() <<
+      //  "  n CSSD jets: " << jetCollectionCSSDs[ics].getJet().size() << std::endl;
+    }
+    
+    
+    //match the CS jets to signal jets
+    for(int ics = 0; ics<ncs; ++ics) {
+      jetMatcher jmCS(R);
+      jmCS.setBaseJets(jetCollectionCSs[ics]);
+      jmCS.setTagJets(jetCollectionSig);
+      jmCS.matchJets();
+
+      jmCS.reorderedToTag(jetCollectionCSs[ics]);
+      jmCS.reorderedToTag(jetCollectionCSSDs[ics]);
+    }
+
+    //---------------------------------------------------------------------------
+    //   write tree
+    //---------------------------------------------------------------------------
+    
+    //Give variable we want to write out to treeWriter.
+    //Only vectors of the types 'jetCollection', and 'double', 'int', 'fastjet::PseudoJet' are supported
+
+    trw.addCollection("sigJet",        jetCollectionSig);
+    trw.addCollection("sigJetSD",      jetCollectionSigSD);
+
+    for(int ics = 0; ics<ncs; ++ics) {
+      trw.addCollection(Form("csJet_%d",ics),         jetCollectionCSs[ics]);
+      trw.addCollection(Form("csJetSD_%d",ics),       jetCollectionCSSDs[ics]);
+    }
+    trw.addCollection("csRho",         rho);
+    trw.addCollection("csRhom",        rhom);
+
+    trw.addCollection("eventWeight",   eventWeight);
+        
+    trw.fillTree();
+
+  }//event loop
+
+  Bar.Update(nEvent);
+  Bar.Print();
+  Bar.PrintLine();
+
+  TTree *trOut = trw.getTree();
+
+  TFile *fout = new TFile("JetToyHIResultCSVariations.root","RECREATE");
+  trOut->Write();
+  fout->Write();
+  fout->Close();
+
+  double time_in_seconds = std::chrono::duration_cast<std::chrono::milliseconds>
+    (std::chrono::steady_clock::now() - start_time).count() / 1000.0;
+  std::cout << "runFromFile: " << time_in_seconds << std::endl;
+}
