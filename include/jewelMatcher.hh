98f4484d4e73d7ac4fb88848693809d313179ba7
Yi <chen.yi.first@gmail.com>
2017-08-30 14:58:30 +0200

    bug fix

--- include/jewelMatcher.hh
+++ include/jewelMatcher.hh
@@ -6,24 +6,26 @@
 
 #include "fastjet/PseudoJet.hh"
 
-fastjet::PseudoJet GetCorrection(std::vector<fastjet::PseudoJet> &Constituents, std::vector<fastjet::PsuedoJet> ThermalParticles);
-fastjet::PseudoJet GetJetCorrection(fastjet::PseudoJet Jet, std::vector<fastjet::PsuedoJet> ThermalParticles);
-fastjet::PseudoJet GetCorrectedJet(fastjet::PseudoJet Jet, std::vector<fastjet::PsuedoJet> ThermalParticles);
-std::vector<fastjet::PseudoJet> GetCorrectedJets(std::vector<fastjet::PseudoJet> Jets, std::vector<fastjet::PsuedoJet> ThermalParticles);
+fastjet::PseudoJet GetCorrection(std::vector<fastjet::PseudoJet> Constituents, std::vector<fastjet::PseudoJet> ThermalParticles);
+fastjet::PseudoJet GetJetCorrection(fastjet::PseudoJet Jet, std::vector<fastjet::PseudoJet> ThermalParticles);
+fastjet::PseudoJet GetCorrectedJet(fastjet::PseudoJet Jet, std::vector<fastjet::PseudoJet> ThermalParticles);
+std::vector<fastjet::PseudoJet> GetCorrectedJets(std::vector<fastjet::PseudoJet> Jets, std::vector<fastjet::PseudoJet> ThermalParticles);
 
-fastjet::PseudoJet GetCorrection(std::vector<fastjet::PseudoJet> &Constituents, std::vector<fastjet::PsuedoJet> ThermalParticles)
+fastjet::PseudoJet GetCorrection(std::vector<fastjet::PseudoJet> Constituents, std::vector<fastjet::PseudoJet> ThermalParticles)
 {
    fastjet::PseudoJet Correction;
 
    for(fastjet::PseudoJet p : Constituents)
    {
-      if(p.E() > 0.0001)   // definitely not a dummy - this should speed things up a lot
+      if(p.E() > 0.01)   // definitely not a dummy - this should speed things up a lot
          continue;
 
       for(fastjet::PseudoJet &j : ThermalParticles)
       {
-         if(p.squared_distance(j) > 1e-10)
+         if(p.squared_distance(j) > 1e-8)
             continue;
+
+         std::cout << "Yay" << std::endl;
 
          Correction = Correction + j;
          j.reset(0, 0, 0, 0);
@@ -33,19 +35,19 @@ fastjet::PseudoJet GetCorrection(std::vector<fastjet::PseudoJet> &Constituents, 
    return Correction;
 }
 
-fastjet::PseudoJet GetJetCorrection(fastjet::PseudoJet Jet, std::vector<fastjet::PsuedoJet> ThermalParticles)
+fastjet::PseudoJet GetJetCorrection(fastjet::PseudoJet Jet, std::vector<fastjet::PseudoJet> ThermalParticles)
 {
    fastjet::PseudoJet Correction = GetCorrection(Jet.constituents(), ThermalParticles);
    return Correction;
 }
 
-fastjet::PseudoJet GetCorrectedJet(fastjet::PseudoJet Jet, std::vector<fastjet::PsuedoJet> ThermalParticles)
+fastjet::PseudoJet GetCorrectedJet(fastjet::PseudoJet Jet, std::vector<fastjet::PseudoJet> ThermalParticles)
 {
    fastjet::PseudoJet Correction = GetCorrection(Jet.constituents(), ThermalParticles);
    return (Jet - Correction);
 }
 
-std::vector<fastjet::PseudoJet> GetCorrectedJets(std::vector<fastjet::PseudoJet> Jets, std::vector<fastjet::PsuedoJet> ThermalParticles)
+std::vector<fastjet::PseudoJet> GetCorrectedJets(std::vector<fastjet::PseudoJet> Jets, std::vector<fastjet::PseudoJet> ThermalParticles)
 {
    std::vector<fastjet::PseudoJet> Result;
    for(fastjet::PseudoJet j : Jets)
