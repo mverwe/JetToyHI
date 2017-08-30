d4263b2de847b454b5ac9d4697959396cd2564f2
Yi <chen.yi.first@gmail.com>
2017-08-30 14:30:05 +0200

    jewel subtraction code update

--- include/jewelMatcher.hh
+++ include/jewelMatcher.hh
@@ -2,10 +2,14 @@
 #define jewelMatcher_h
 
 #include <iostream>
+#include <vector>
 
 #include "fastjet/PseudoJet.hh"
 
 fastjet::PseudoJet GetCorrection(std::vector<fastjet::PseudoJet> &Constituents, std::vector<fastjet::PsuedoJet> ThermalParticles);
+fastjet::PseudoJet GetJetCorrection(fastjet::PseudoJet Jet, std::vector<fastjet::PsuedoJet> ThermalParticles);
+fastjet::PseudoJet GetCorrectedJet(fastjet::PseudoJet Jet, std::vector<fastjet::PsuedoJet> ThermalParticles);
+std::vector<fastjet::PseudoJet> GetCorrectedJets(std::vector<fastjet::PseudoJet> Jets, std::vector<fastjet::PsuedoJet> ThermalParticles);
 
 fastjet::PseudoJet GetCorrection(std::vector<fastjet::PseudoJet> &Constituents, std::vector<fastjet::PsuedoJet> ThermalParticles)
 {
@@ -27,6 +31,26 @@ fastjet::PseudoJet GetCorrection(std::vector<fastjet::PseudoJet> &Constituents, 
    }
 
    return Correction;
+}
+
+fastjet::PseudoJet GetJetCorrection(fastjet::PseudoJet Jet, std::vector<fastjet::PsuedoJet> ThermalParticles)
+{
+   fastjet::PseudoJet Correction = GetCorrection(Jet.constituents(), ThermalParticles);
+   return Correction;
+}
+
+fastjet::PseudoJet GetCorrectedJet(fastjet::PseudoJet Jet, std::vector<fastjet::PsuedoJet> ThermalParticles)
+{
+   fastjet::PseudoJet Correction = GetCorrection(Jet.constituents(), ThermalParticles);
+   return (Jet - Correction);
+}
+
+std::vector<fastjet::PseudoJet> GetCorrectedJets(std::vector<fastjet::PseudoJet> Jets, std::vector<fastjet::PsuedoJet> ThermalParticles)
+{
+   std::vector<fastjet::PseudoJet> Result;
+   for(fastjet::PseudoJet j : Jets)
+      Result.push_back(GetCorrectedJet(j, ThermalParticles));
+   return Result;
 }
 
 #endif
