f835ae241b8e620aee9d1fd0b928c300b90cd645
Leticia Cunqueiro Mendez <lcunquei@lxplus029.cern.ch>
2019-05-14 15:51:37 +0200

    modifying the setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+
 source /cvmfs/sft.cern.ch/lcg/external/gcc/4.8.1/x86_64-slc6-gcc48-opt/setup.sh
 source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.06.08/x86_64-slc6-gcc48-opt/root/bin/thisroot.sh
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/cvmfs/sft.cern.ch/lcg/external/MCGenerators_lcgcmt67c/pythia8/201/x86_64-slc6-gcc48-opt/lib 
